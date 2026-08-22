f9f7a02e5c2b8dffb1c4081769cace4d8ddb63c6
Shivam Raj <142863437+Shivamparth11@users.noreply.github.com>
2026-08-22 06:57:08 +0530

    Time: 15 ms (9.72%), Space: 22.7 MB (51.63%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,33 +1,33 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        cnt1,cnt2 = 0,0
+        cnt1,cnt2 =0,0
         el1 = None
         el2 = None
         for i in range(n):
-            if(cnt1 == 0 and el2 != nums[i]):
+            if(cnt1==0 and el2 != nums[i]):
                 cnt1 = 1
                 el1 = nums[i]
             elif(cnt2 == 0 and el1 != nums[i]):
                 cnt2 = 1
                 el2 = nums[i]
-            elif(nums[i]==el1):
+            elif(el1==nums[i]):
                 cnt1+=1
-            elif(nums[i]==el2):
+            elif(el2 == nums[i]):
                 cnt2+=1
-            else:
+            else: 
                 cnt1-=1
                 cnt2-=1
         cnt1,cnt2 = 0,0
         for i in range(n):
-            if(el1 == nums[i]):
+            if(el1==nums[i]):
                 cnt1+=1
-            if(el2 == nums[i]):
+            if(el2==nums[i]):
                 cnt2+=1
-        max = (n//3)
         ls = []
-        if(cnt1 > max):
+        max = n//3
+        if(cnt1>max):
             ls.append(el1)
-        if(cnt2 > max):
+        if(cnt2>max):
             ls.append(el2)
         return ls
